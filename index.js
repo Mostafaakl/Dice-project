6f76b901105946224bdb326bf576ba117e9717e0
Mostafaakl <161155013+Mostafaakl@users.noreply.github.com>
2024-04-03 22:25:24 +0200

    index.js

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ var randomDiceimage2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1]
 image2.setAttribute("src" , randomDiceimage2)
 
-    if (randomNumber1> randomNumber2){
+    if (randomNumber1> randomNumber2) {
         document.querySelector("h1").innerHTML= "Player 1 Won";
     }
-    else if (randomNumber1< randomNumber2) {
+    else if (randomNumber1 > randomNumber2) {
         document.querySelector("h1").innerHTML= "Player 2 Won";
-
     }
     else{
         document.querySelector("h1").innerHTML= "Draw";
-
     }
 
