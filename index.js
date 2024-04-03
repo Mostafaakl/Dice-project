c2eb9bdd36e0aae9a77bcc4c43efdc834bb704b2
Mostafaakl <161155013+Mostafaakl@users.noreply.github.com>
2024-04-03 22:14:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = (Math.floor(Math.random() * 6)+1);
+var randomDiceimage1 = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0]
+image1.setAttribute("src",randomDiceimage1);
+
+
+var randomNumber2 = (Math.floor(Math.random() * 6)+1);
+var randomDiceimage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1]
+image2.setAttribute("src" , randomDiceimage2)
+
+function winner (randomNumber1 , randomNumber2 ) {
+    if (randomNumber1> randomNumber2){
+        document.querySelector("h1").innerHTML= "Player 1 Won";
+    }
+    else if (randomNumber1< randomNumber2) {
+        document.querySelector("h1").innerHTML= "Player 2 Won";
+
+    }
+    else{
+        document.querySelector("h1").innerHTML= "Draw";
+
+    }
+}
